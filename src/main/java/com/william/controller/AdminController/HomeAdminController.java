9636cb53bf3c594a89fa588e6d6a902969a883e0
WilliamNguyen <williamnguyen8888@gmail.com>
2021-01-11 10:14:51 +0700

    commit new pack

--- src/main/java/com/william/controller/AdminController/HomeAdminController.java
+++ src/main/java/com/william/controller/AdminController/HomeAdminController.java
@@ -1,13 +1,14 @@
 package com.william.controller.AdminController;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/admin")
 public class HomeAdminController {
-    @RequestMapping("/")
+    @GetMapping
     public ModelAndView home(){
         ModelAndView modelAndView = new ModelAndView("/backend/index");
         return modelAndView;
