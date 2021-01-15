4c2085bfa9434a6c1638441d7f6ef2f2a706fc12
NT <nt.tugg@gmail.com>
2021-01-15 12:56:39 +0700

    add session to user pages

--- src/main/java/com/william/controller/DetailVideoController.java
+++ src/main/java/com/william/controller/DetailVideoController.java
@@ -1,5 +1,6 @@
 package com.william.controller;
 
+import com.william.entity.AccountEntity;
 import com.william.entity.VideosEntity;
 import com.william.repository.IVideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 @Controller
@@ -16,7 +18,11 @@ public class DetailVideoController {
     @Autowired
     private IVideoRepository videoRepository;
     @RequestMapping("/detail")
-    public String detailVideo(@RequestParam(value = "idVideos",required = false) String id, Model model) {
+    public String detailVideo(@RequestParam(value = "idVideos",required = false) String id, Model model, HttpSession session) {
+        Optional<AccountEntity> user = (Optional<AccountEntity>) session.getAttribute("userLogged");
+        if(user != null){
+            model.addAttribute("user", user.get());
+        }
         if (id != null) {
             Optional<VideosEntity> videosEntity = videoRepository.findById(Integer.parseInt(id));
             if (videosEntity!=null){
